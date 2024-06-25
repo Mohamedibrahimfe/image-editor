70bb139fbea8b4e174fd466e1c1f89a17a826e9b
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-06-25 04:56:38 +0300

    handle all functionalities

--- js/main.js
+++ js/main.js
@@ -34,22 +34,47 @@ inputPic.addEventListener('change', () => {
 
 // handle manipulating
 let saturate = document.getElementById("saturate")
-let Contrast = document.getElementById("Contrast")
-let Brightness = document.getElementById("Brightness")
-let Sepia = document.getElementById("Sepia")
-let Grayscale = document.getElementById("Grayscale")
-let blur = document.getElementById("Blur")
-let hueRotate= document.getElementById("hueRotate")
+let contrast = document.getElementById("contrast")
+let brightness = document.getElementById("brightness")
+let Sepia = document.getElementById("sepia")
+let grayscale = document.getElementById("grayscale")
+let blur = document.getElementById("blur")
+let hueRotate= document.getElementById("hue-rotate")
+let canvas = document.getElementById("canvas")
+const ctx = canvas.getContext('2d')
 
 
-
-
-
-
-
-
-
-
+let filters = document.querySelectorAll("ul li input");
+filters.forEach(filter => {
+    filter.addEventListener('input', function(){
+        ctx.filter = `
+        saturate(${saturate.value}%)
+        contrast(${contrast.value}%)
+        brightness(${brightness.value}%)
+        sepia(${sepia.value}%)
+        grayscale(${grayscale.value})
+        blur(${blur.value}px)
+        hue-rotate(${hueRotate.value}deg)
+        `
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+    })
+})
 // handle reset 
-let reset = document.querySelector('reset')
+document.getElementById('reset').onclick = ()=>{
+    img.style.filter = 'none'
+    saturate.value = '100';
+    contrast.value = '100';
+    brightness.value = '100';
+    sepia.value = '0';
+    grayscale.value = '0';
+    blur.value = '0';
+    hueRotate.value = '0';
+}
 
+
+
+// handle download
+let download = document.querySelector('#download')
+download.onclick = function(){
+    download.href = canvas.toDataURL()
+}
